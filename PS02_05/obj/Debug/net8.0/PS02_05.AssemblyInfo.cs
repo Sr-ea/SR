4607fb9a5da013dd6b56262905ff1a99dd3615c6
Rhea <svynry@gmail.com>
2024-08-28 01:41:23 +0800

    .

--- PS02_05/obj/Debug/net8.0/PS02_05.AssemblyInfo.cs
+++ PS02_05/obj/Debug/net8.0/PS02_05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PS02_05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6954f5aa43c7f4824c288ed9fa6a6f078b146bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdec2ffe628da7d0a4855b8c7ac5b8276b1b51c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PS02_05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PS02_05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
