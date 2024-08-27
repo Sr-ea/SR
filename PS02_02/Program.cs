4ab1139a819c79af1fae67bcabe18f76552b3fcf
Rhea Lee Salonoy <svynry@gmail.com>
2024-08-27 20:51:07 +0800

    Update Program.cs

--- PS02_02/Program.cs
+++ PS02_02/Program.cs
@@ -10,9 +10,11 @@ namespace PS02_02
     {
         static void Main(string[] args)
         {
-            /*
-            * Write a program that prints all the odd numbers from 1 to 20 using a for loop. 
-            */
+           int num1 = 20;
+            for (int i = 1; i < num1; i += 2)
+            { 
+                Console.WriteLine(i); 
+            }   
         }
     }
 }
